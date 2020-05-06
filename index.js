751c5d7489466926012d5421748b7ffde207212f
ajagetia2001 <jagetiaaaryan06@gmail.com>
2020-05-06 16:15:42 +0530

    express example 1

---
+++ index.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const http =require('http');
+const app=express();
+const hostname='localhost';
+const port=3000;
+app.use((req, res, next) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+  
+  });
+const server=http.createServer(app);
+server.listen(port,hostname,()=>{
+console.log(`server running at http://${hostname}:${port}`);
+})
